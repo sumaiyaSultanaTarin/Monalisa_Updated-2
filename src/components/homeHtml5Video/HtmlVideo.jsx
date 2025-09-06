729deaaa6dffd1d9bb4e1cad1d7858530c7ff55a
Demo <sumaiyaatarin11@gmail.com>
2025-09-07 01:22:33 +0600

    Fixed video scaling issue and added accessibility

--- src/components/homeHtml5Video/HtmlVideo.jsx
+++ src/components/homeHtml5Video/HtmlVideo.jsx
@@ -4,13 +4,14 @@ export default function HtmlVideo() {
   return (
     <>
       <section id="home" className="html-video">
-			<video className="text-center" muted autoPlay loop playsInline>
+			<video className="video-bg" muted autoPlay loop playsInline>
 				{/* WebM/VP8 for Firefox4, Opera, and Chrome */}
 				<source type="video/webm" src="assets/videos/video.webm"/>
 				{/* MP4 for Safari, IE9, iPhone, iPad, Android, and Windows Phone 7 */}
 				<source type="video/mp4" src="assets/videos/video.mp4"/>
 				Your browser does not support the video tag.
 			</video> 
+			<div  className="video-overlay"></div>
 			<div className="home_text slider-caption text-center">
 				  <h2 className="animated fadeInDownBig">Welcome our Monalisa</h2>
 				  <p className="animated fadeInLeftBig">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae risus nec dui venenatis dignissim. Aenean vitae metus in augue pretium ultrices. Duis dictum eget dolor vel blandit.</p>
